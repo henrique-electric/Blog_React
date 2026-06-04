bfa148b8b04a70e3fb4f5b5127637e0316b89007
José Henrique <146463048+henrique-electric@users.noreply.github.com>
2026-06-04 12:00:49 -0300

    Start to build the main page of the frontend

--- frontend/src/mainPage.jsx
+++ frontend/src/mainPage.jsx
@@ -4,8 +4,7 @@ export default function MainPage() {
     return (
         <div className="mainPage">
             <div className="mainContent">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt et doloribus maiores animi consequuntur voluptatum explicabo facere nobis magnam sit dicta excepturi cum, quod provident ea perferendis. Maiores, nisi aspernatur.</p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, ab cupiditate? Mollitia repellat culpa, vitae architecto velit, quae voluptatibus sequi laboriosam sint beatae vero enim itaque doloremque nulla harum pariatur!
+                <p>Esse blog tem como objetivo de eu documentar aquilo que eu aprendo </p>
             </div>
         </div>
             
